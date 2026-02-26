19aaf7db15bcd590585f53c00b6c66bcdc591e4e
Letícia <leticiacastao133@gmail.com>
2026-02-25 22:17:48 -0300

    Aula 2 - Operadores

---
+++ Aula2/src/script02.js
@@ -0,0 +1,29 @@
+//Exemplo de operação aritmética
+let a = 5;
+let b = (100 + 200) * a;
+
+console.log(b);
+
+//Exemplo de adição
+
+let texto1 = "Somente a dor coletiva";
+//texto1 += " gera união!";
+texto1 = texto1 + " gera união!"; //mesma coisa que a linha de cima
+
+console.log(texto1);
+
+//Exemplo de Incremento
+
+let w = 5;
+w++; 
+
+let z = w;
+console.log(z);
+
+//Exemplo de Decremento
+
+let c = 5;
+b--;
+let d = c;
+console.log(d);
+
