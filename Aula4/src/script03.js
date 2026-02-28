1d8bbcbac6da96aa63b9eb93b5bfb58d008d990b
Letícia <leticiacastao133@gmail.com>
2026-02-27 22:38:19 -0300

    Aula 4 - Condições Lógicas (Else, Switch e Ternário)

---
+++ Aula4/src/script03.js
@@ -0,0 +1,7 @@
+//Exemplos de condição ternária
+let idade = 20;
+let texto = (idade > 18) ? "Adulto!" : "Menor!";
+//let texto2 = (idade > 18) ? "true" : "false!";
+
+document.getElementById("ex1").innerHTML = texto;
+
