6228c5eecaab52d9d4960cc4691e676977ee335c
Letícia <leticiacastao133@gmail.com>
2026-03-12 19:55:19 -0300

    Aula 12 - Construtores de Objetos

---
+++ Aula12/src/script.js
@@ -0,0 +1,52 @@
+// Criando um construtor
+
+function Pessoa(primeiroNome, ultimoNome, idade, pais) {
+  this.primeiroNome = primeiroNome;
+  this.ultimoNome = ultimoNome;
+  this.idade = idade;
+  this.pais = pais;
+}
+
+const euMesmo = new Pessoa("Letícia", "Castão", 20, "Brasil");
+
+const meuMae = new Pessoa("Santos", "Eliene", 38, "Brasil");
+
+// console.log(euMesmo);
+// console.log(meuIrmao);
+
+// Reforçando criação de construtores de objetos
+function Veiculo(marca, modelo, motorPotencia, cor, ano, rodas) {
+  this.marca = marca;
+  this.modelo = modelo;
+  this.motorPotencia = motorPotencia;
+  this.cor = cor;
+  this.ano = ano,
+  this.rodas = rodas;
+  this.acelerar = function() {
+    return this.modelo + " acelerou.";
+  };
+}
+
+const minhaMoto = new Veiculo("Ducati", "Panigale", "216cv", "Azul", 2019, 2);
+
+console.log(minhaMoto);
+
+const meuCarro = new Veiculo("Porsche", "944", "240cv", "Branco", 1988, 4);
+
+console.log(meuCarro);
+
+meuCarro.placa = "0000-0000";
+
+meuCarro.frear = function() {
+  return this.modelo + " freou."
+};
+
+console.log(meuCarro.frear());
+
+console.log(meuCarro.placa)
+
+Veiculo.prototype.parar = function() {
+  return this.marca + " parou."
+}
+
+console.log(meuCarro.parar());
