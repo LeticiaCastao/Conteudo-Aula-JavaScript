16ddfbd8f67205459ae139aa2a7e19f001a95c01
Letícia <leticiacastao133@gmail.com>
2026-03-19 19:01:01 -0300

    Aula 15 - Estudando Manipulações de Elementos no HTML

---
+++ Aula15/src/script03.js
@@ -0,0 +1,7 @@
+// Utilizando o innerHTML
+document.querySelector(".mudandoElemento").innerHTML = "Cássia Ribeiro";
+
+// Alterando atributos
+const elemento = document.querySelector(".foto");
+
+elemento.src = "Aula15/img/download (1).jpg";
